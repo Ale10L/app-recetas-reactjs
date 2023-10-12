f59c44c40765b11ca1afe8d6efeb55ab90e9e1e7
Alejandro Ghiringhelli <alejandroghiringhelli@gmail.com>
2023-10-12 11:39:49 -0300

    agregando formulario de contacto de prueba deploy axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
 import './fuentes/Lemon-Regular.ttf'
+import ContactForm from './ContactForm';
 
 function App({ children }) {
   return (
     <div className="">
+      <ContactForm />
       <header className=''>
         <Header />
       </header>
